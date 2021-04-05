2c2b745c484d923bf40c928ce2e03b8c9c0ecba3
Haskor <shahar.haskor@vayyar.com>
2021-04-05 13:57:43 +0300

    first tests

--- TC_folder/main.py
+++ TC_folder/main.py
@@ -1,22 +1,26 @@
 import random
+import sys
 
 
 def test1(param1):
-    r = random.randint(100)
+    print('test1')
+    r = random.randint(1, 100)
     print(f'number generated is {r}')
     assert r < param1
 
 
 def test2(param1):
-    r = random.randint(200)
+    print('test2')
+    r = random.randint(1, 200)
     print(f'number generated is {r}')
     assert r < param1
 
 
 if __name__ == '__main__':
-    param1 = sys.argv[1]
+    param1 = int(sys.argv[1])
     print('hello there')
     print(f'param is {param1}')
     print('begin tests')
     test1(param1)
     test2(param1)
+    print('finished tests')
